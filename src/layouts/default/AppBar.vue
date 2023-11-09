63da90278c66959266c13bc9980a3636cbb0a6b0
Ricardo Castro <ricardocastrodesign@gmail.com>
2023-11-09 20:53:18 +0000

    change default vuetify theme

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,9 +1,8 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
+      <v-icon icon="mdi-check-circle-outline" />
+      Todo App
     </v-app-bar-title>
   </v-app-bar>
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home pa-6">
+    <h1>Todo page</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
