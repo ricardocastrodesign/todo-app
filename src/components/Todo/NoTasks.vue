45bf3630af01dbbf491924e3e992c7a97355ea1f
Ricardo Castro <ricardocastrodesign@gmail.com>
2023-11-10 11:45:03 +0000

    add no tasks component and validation

---
+++ src/components/Todo/NoTasks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="no-tasks">
+    <v-icon size="100">mdi-check</v-icon>
+    <p>All done for now!</p>
+
+  </div>
+</template>
+
+<style scoped>
+.no-tasks {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="home">
-    <TasksList />
+    <AddTask />
+    <template v-if="tasks.length > 0">
+      <TasksList />
+    </template>
+    <template v-else>
+      <NoTasks />
+    </template>
   </div>
 </template>
 
 <script>
+import { useTasks } from "@/store/tasks";
+
+import AddTask from "@/components/Todo/AddTask.vue";
 import TasksList from "@/components/Todo/TasksList.vue";
+import NoTasks from "@/components/Todo/NoTasks.vue";
 export default {
   components: {
     TasksList,
+    AddTask,
+    NoTasks
+  },
+  mounted() {
+    console.log(this.$store);
+
+  },
+  computed: {
+    tasks() {
+      return useTasks().filteredTasks;
+    },
   },
 };
 </script>
