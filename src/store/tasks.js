3f269544ad7c8a8b0c38f2549c0f5c782271f713
Ricardo Castro <ricardocastrodesign@gmail.com>
2023-11-10 18:13:06 +0000

    edit task api

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -49,8 +49,17 @@ export const useTasks = defineStore('tasks', {
         console.error('Error toggling task:', error);
       }
     },
-    editTask(payload) {
-      console.log('Edit Task:', payload);
+    async editTask(payload) {
+      try {
+        await api.updateTask(payload.id, payload);
+        
+        const taskIndex = this.tasks.findIndex((task) => task.id === payload.id);
+        if (taskIndex !== -1) {
+          this.tasks[taskIndex] = { ...this.tasks[taskIndex], ...payload };
+        }
+      } catch (error) {
+        console.error('Error updating task:', error);
+      }
     },
     deleteTask(id) {
       this.tasks = this.tasks.filter((task) => task.id !== id);
